f1ede23dcd335d66ad9e1e93d66adfd1f68021e7
ondervolkan <ondervolkansci@gmail.com>
2019-05-28 18:00:25 +0300

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+import 'package:FirstDartPro/FirstDartPro.dart' as FirstDartPro;
+
+main(List<String> arguments) {
+  print('Hello world: ${FirstDartPro.calculate()}!');
+
+  sayPurpose();
+
+}
+
+sayPurpose(){
+  print("Hello! This is a first Dart Project.");
+}
---
+++ test/FirstDartPro_test.dart
@@ -0,0 +1,8 @@
+import 'package:FirstDartPro/FirstDartPro.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
